0732c3ba52aa247f6846376813801b27e8500524
MikePourbaix <165761185+MikePourbaix@users.noreply.github.com>
2024-10-03 17:35:24 +0200

    db patch

--- models/db_conf.js
+++ models/db_conf.js
@@ -3,4 +3,4 @@
 const db = require('better-sqlite3')('db/exoplanets.db', { verbose: console.log });
 // const db = require('better-sqlite3')('/home/olivier/exoplanets.db', { verbose: console.log });
 
-// module.exports = db;
+module.exports = db;
