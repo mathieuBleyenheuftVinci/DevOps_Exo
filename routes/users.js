1677346ff35b5e8a7ed6aa9e55586484f55bb92c
mathieuBleyenheuftVinci <165761155+mathieuBleyenheuftVinci@users.noreply.github.com>
2024-10-10 16:22:00 +0200

    check

--- routes/users.js
+++ routes/users.js
@@ -28,23 +28,7 @@ router.post('/login', (req, res, next) => {
             res.redirect('/users');
         }
         else {
-            if (bcrypt.compareSync(req.body.userPassword, userFound.password)) {
-                console.log("password correct");
-                req.session.login = req.body.userLogin;
-                req.session.connected = true;
-                if (userFound.admin) {
-                    req.session.admin = true;
-                    res.redirect('/admin');
-                } else {
-                    req.session.admin = false;
-                    res.redirect('/members');
-                }
-            }
-            else {
-                console.log("bad password");
-                req.session.errors = "Mot de passe incorrect";
-                res.redirect('/users');
-            }
+            FunctioncheckPassword(req, userFound, res);
         }
     }
     else {
@@ -96,3 +80,23 @@ router.post('/add', (req, res, next) => {
 });
 
 module.exports = router;
+
+function FunctioncheckPassword(req, userFound, res) {
+    if (bcrypt.compareSync(req.body.userPassword, userFound.password)) {
+        console.log("password correct");
+        req.session.login = req.body.userLogin;
+        req.session.connected = true;
+        if (userFound.admin) {
+            req.session.admin = true;
+            res.redirect('/admin');
+        } else {
+            req.session.admin = false;
+            res.redirect('/members');
+        }
+    }
+    else {
+        console.log("bad password");
+        req.session.errors = "Mot de passe incorrect";
+        res.redirect('/users');
+    }
+}
